173cf3590b169736e26ad19a74b7e26ef906cb51
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-07-26 23:11:26 -0300

    Atualizando tsconfig por erro ao compilar

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss']
 })
-export class NovaTransferenciaComponent implements OnInit {
+export class NovaTransferenciaComponent {
+
+  valor: number = 0;
+  destino: number = 0;
 
   transferir() {
-    console.log("oi")
-  }
-  constructor() { }
-
-  ngOnInit(): void {
-
+    console.log('Solicitada nova transferência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
   }
 
 }
