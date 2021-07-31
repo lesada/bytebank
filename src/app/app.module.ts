4b90324b1085154fc6daada72ccd0e5fed254880
Lauren Leslie <laurenlesliesilva@gmail.com>
2021-07-30 23:48:20 -0300

    Deixando moeda em pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
-import {CommonModule, registerLocaleData} from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
 
@@ -16,15 +16,16 @@ registerLocaleData(localePt, 'pt')
     AppComponent,
     NovaTransferenciaComponent,
     ExtratoComponent,
-
-
   ],
   imports: [
     BrowserModule,
     FormsModule,
     CommonModule
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt' }],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
